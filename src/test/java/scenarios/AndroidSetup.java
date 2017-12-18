d49807af345b14e8261c25f16641144beb5a3a21
Stepan Kazantsev <maver1kxvii@gmail.com>
2017-12-18 19:00:31 +0700

    Changed TestNG to JUnit, added APK

--- src/test/java/scenarios/AndroidSetup.java
+++ src/test/java/scenarios/AndroidSetup.java
@@ -1,10 +1,16 @@
 package scenarios;
 
 import io.appium.java_client.android.AndroidDriver;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 public class AndroidSetup {
     protected AndroidDriver driver;
@@ -13,11 +19,38 @@ public class AndroidSetup {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("appium-version", "1.0");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "5.0");
-        capabilities.setCapability("deviceName", "Nexus");
-        capabilities.setCapability("app", "/Users/nishant/Development/AppiumDemo/apps/flipkart.apk");
-        capabilities.setCapability("appPackage", "com.flipkart.android");
-        capabilities.setCapability("appActivity", "com.flipkart.android.SplashActivity");
+        capabilities.setCapability("platformVersion", "4.4");
+        capabilities.setCapability("deviceName", "Emu01");
+        capabilities.setCapability("app", "/home/skaz17/myGitRepos/appiumTest/Appium-test-Incubator/apps/info-goodline-btv.391.apk");
+        capabilities.setCapability("appPackage", "info.goodline.btv");
+        capabilities.setCapability("appActivity", "info.goodline.btv.ui.activity.AuthActivity");
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+    }
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        prepareAndroidForAppium();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void helloTest()
+    {
+        String app_package_name = "info.goodline.btv";
+        /*By userId = By.id(app_package_name + "user_id");
+        By password = By.id(app_package_name + "user_password");
+        By showPassword = By.id(app_package_name + "show_password");
+        By login_Button = By.id(app_package_name + "btn_login");*/
+
+        /*driver.findElement(userId).sendKeys("someone@testvagrant.com");
+        driver.findElement(password).sendKeys("testvagrant123");
+        driver.findElement(showPassword).click();
+        driver.findElement(login_Button).click();*/
+
+        System.out.println(app_package_name);
     }
 }
