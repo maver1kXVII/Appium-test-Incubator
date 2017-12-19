2b8b158a0c24c9340312bde6ee40a44366a8b435
Stepan Kazantsev <maver1kxvii@gmail.com>
2017-12-19 15:55:55 +0700

    Activities

--- src/test/java/scenarios/AndroidSetup.java
+++ src/test/java/scenarios/AndroidSetup.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -11,9 +12,22 @@ import org.openqa.selenium.By;
 import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 public class AndroidSetup {
     protected static AndroidDriver driver;
+
+    WebDriverWait wait;
+
+    //кнопка в левом верхнем углу ("меню" или "назад")
+    By menuBtn = By.xpath("//android.widget.ImageButton[contains(@index, '0')]");
+    //кнопка "Любимые"
+    By favBtn = By.xpath("//android.widget.ImageView[contains(@index, '0')]");
+
+    String activityMain = ".ui.activity.MainActivity";
 
     protected static void prepareAndroidForAppium() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -24,6 +38,7 @@ public class AndroidSetup {
         capabilities.setCapability("app", "/home/skaz17/myGitRepos/appiumTest/Appium-test-Incubator/apps/info-goodline-btv.391.apk");
         capabilities.setCapability("appPackage", "info.goodline.btv");
         capabilities.setCapability("appActivity", "info.goodline.btv.ui.activity.AuthActivity");
+        //capabilities.setCapability("appActivity", "info.goodline.btv.ui.activity.MainActivity");
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
     }
 
@@ -40,17 +55,41 @@ public class AndroidSetup {
     @Test
     public void helloTest()
     {
-        String app_package_name = "info.goodline.btv";
-        /*By userId = By.id(app_package_name + "user_id");
-        By password = By.id(app_package_name + "user_password");
-        By showPassword = By.id(app_package_name + "show_password");
-        By login_Button = By.id(app_package_name + "btn_login");*/
-
+        //By password = By.id(app_package_name + "user_password");
         /*driver.findElement(userId).sendKeys("someone@testvagrant.com");
-        driver.findElement(password).sendKeys("testvagrant123");
-        driver.findElement(showPassword).click();
-        driver.findElement(login_Button).click();*/
+        driver.findElement(password).sendKeys("testvagrant123");*/
 
-        System.out.println(app_package_name);
+        waitForActivity(activityMain, 10);
+        assertEquals(activityMain, driver.currentActivity());
+
+        driver.findElement(menuBtn).click();
+
+        wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.elementToBeClickable(favBtn));
+        driver.findElement(favBtn).click();
+
+        wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.elementToBeClickable(menuBtn));
+        assertEquals(".ui.activity.SearchBarActivity", driver.currentActivity());
+
+        driver.findElement(menuBtn).click();
     }
+
+    public void waitForActivity(String activityName, int timeout)
+    {
+        int counter = 0;
+        do {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            counter++;
+        } while(!(driver.currentActivity().contains(activityName)) && (counter<=timeout));
+    }
+
+    /*public void reportCurrentActivity()
+    {
+        System.out.println("Current activity: " + driver.currentActivity());
+    }*/
 }
